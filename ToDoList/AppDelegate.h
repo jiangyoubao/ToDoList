61b5c85570f2662afe2aae98273e09b0dd201c2f
jiangyoubao <Chris@song-songzhengs-macbook-pro.local>
2015-04-27 09:26:25 +0100

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Song Songzheng on 27/4/15.
+//  Copyright (c) 2015 ssz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Song Songzheng on 27/4/15.
+//  Copyright (c) 2015 ssz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
